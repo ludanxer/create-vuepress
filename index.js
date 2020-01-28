470dacb437830a8b5077d63f16bbd800ec736b05
ludanxer <ludanxer@gmail.com>
2020-01-28 18:28:18 +0800

    merge cli.js into index.js

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-const run = require('./lib/run');
+#!/usr/bin/env node
 
-module.exports.run = run;
+const yParser = require('yargs-parser');
+const semver = require('semver');
+const chalk = require('chalk');
+const run = require('./lib/run');
+const { name, version } = require('./package');
+// const { existsSync } = require('fs');
+// const { join } = require('path');
+
+// Get cli arguments
+const args = yParser(process.argv.slice(2));
+
+// Output version with -v, --version flag
+if (args.v || args.version) {
+  console.log(name, version);
+  // ? what is this for
+  // if (existsSync(join(__dirname, '.local'))) {
+  //   console.log(chalk.cyan('@local'));
+  // }
+  process.exit(0);
+}
+
+if (!semver.satisfies(process.version, '>= 8.0.0')) {
+  console.error(chalk.red('✘ The generator will only work with Node v8.0.0 and up!'));
+  process.exit(1);
+}
+
+const name = args._[0] || '';
+run({
+  name,
+  args,
+});
